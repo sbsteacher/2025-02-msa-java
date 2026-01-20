1730fad2c5cfec79eebf2968356eb807696de80a
sbsteacher <id@gmail.com>
2026-01-20 18:00:37 +0900

    260120 블랙잭 작업 중 (2)

--- src/blackjack/CardDeck.java
+++ src/blackjack/CardDeck.java
@@ -9,29 +9,31 @@ public class CardDeck {
     //Card 객체 주소값 52개를 담을 수 있어야 합니다.
     private Card[] cards = new Card[52];
     private String[] patterns = { "스페이드", "하트", "클로버", "다이아" };
-
     public CardDeck() {
         init();
     }
-
     private void init() {
         int idx = 0;
-        for(int i=0; i<patterns.length; i++) {
+        for(int i=0; i<patterns.length; i++) { //4
             String pattern = patterns[i];
-            for(int d=1; d<=13; d++) {
-                String denomination = String.valueOf(d); //정수 > 문자열
+            for(int d=1; d<=13; d++) { //13
+                //정수 > 문자열
+                String denomination = null;
                 switch(d) {
                     case 1: denomination = "A"; break;
                     case 11: denomination = "J"; break;
                     case 12: denomination = "Q"; break;
                     case 13: denomination = "K"; break;
+                    default: denomination = String.valueOf(d);
                 }
                 Card c = new Card(pattern, denomination);
                 cards[idx++] = c;
             }
         }
-        for(Card c : cards) {
-            System.out.printf("%s - %s\n", c.getPattern(), c.getDenomination());
-        }
+
+//        for(int i=0; i<cards.length; i++) {
+//            Card c = cards[i];
+//            System.out.printf("%s - %s\n", c.getPattern(), c.getDenomination());
+//        }
     }
 }
