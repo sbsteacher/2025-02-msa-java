04f0c54e27d43383549fe3f1b0d333f06e0cf88c
sbsteacher <id@gmail.com>
2026-01-21 11:19:44 +0900

    260120 블랙잭 작업 중 (3)

--- src/blackjack/BlackJackMain.java
+++ src/blackjack/BlackJackMain.java
@@ -3,5 +3,8 @@ package blackjack;
 public class BlackJackMain {
     public static void main(String[] args) {
         CardDeck cd = new CardDeck();
+        Card c1 = cd.draw();
+        System.out.println(c1.getDenomination() + '-' + c1.getPattern());
+        Card c2 = cd.draw();
     }
 }
--- src/blackjack/CardDeck.java
+++ src/blackjack/CardDeck.java
@@ -9,9 +9,25 @@ public class CardDeck {
     //Card 객체 주소값 52개를 담을 수 있어야 합니다.
     private Card[] cards = new Card[52];
     private String[] patterns = { "스페이드", "하트", "클로버", "다이아" };
+    private int drawIdx = 0; //외부로 카드를 준 idx 관리
+
     public CardDeck() {
         init();
+        shuffle(); //카드 섞기
+
     }
+
+    public void print() { //검증용!
+        for(int i=0; i<cards.length; i++) {
+            Card c = cards[i];
+            if(c == null) {
+                System.out.println("null");
+            } else {
+                System.out.printf("%s - %s\n", c.getPattern(), c.getDenomination());
+            }
+        }
+    }
+
     private void init() {
         int idx = 0;
         for(int i=0; i<patterns.length; i++) { //4
@@ -30,10 +46,24 @@ public class CardDeck {
                 cards[idx++] = c;
             }
         }
+    }
+    private void shuffle() {
+        //cards 길이만큼 도는 for문 세팅
+        for(int i=0; i<cards.length; i++) {
+            int randomIdx = (int)(Math.random() * cards.length); // 0~51랜덤값 나올 수 있도록 해주세요.
 
-//        for(int i=0; i<cards.length; i++) {
-//            Card c = cards[i];
-//            System.out.printf("%s - %s\n", c.getPattern(), c.getDenomination());
-//        }
+            //i방과 randomIdx방의 값을 서로 스와핑한다.
+            Card temp = cards[i];
+            cards[i] = cards[randomIdx];
+            cards[randomIdx] = temp;
+        }
+    }
+
+    public Card draw() {
+        if(drawIdx > 51) { return null; } //예외 처리, 안전 장치
+        //cards의 방에 들어있는 card주소값을 순차적으로 리턴해주며 리턴한 방은 null로 바꾼다.
+        Card temp = cards[drawIdx];
+        cards[drawIdx++] = null;
+        return temp;
     }
 }
