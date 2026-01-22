e0a899d2993b8ae5ed60f25c3d232a7a6ca575be
sbsteacher <id@gmail.com>
2026-01-22 17:27:21 +0900

    260122 스타크래프트 마무리

--- src/starcraft/Dropship.java
+++ src/starcraft/Dropship.java
@@ -4,9 +4,33 @@ package starcraft;
 public class Dropship extends Unit {
     //8개의 유닛을 태울 수 있는 공간이 필요
     private Unit[] units = new Unit[8];
+    private int loadIdx = 0;
 
     public void load(Unit unit) {
+        if(loadIdx == units.length) { return; }
+        units[loadIdx++] = unit;
+    }
 
+    public Unit unload(int idx) {
+        Unit temp = units[idx];
+        int len = units.length - 1;
+        for(int i=idx; i<len; i++) {
+            int next = i + 1;
+            if(units[next] == null) { break; }
+            units[i] = units[next];
+            units[next] = null;
+        }
+        return temp;
+    }
+
+    public Unit unload2(int idx) {
+        Unit temp = units[idx];
+        for(int i=idx + 1; i<units.length; i++) {
+            if(units[i] == null) { break; }
+            units[i - 1] = units[i];
+        }
+        units[--loadIdx] = null;
+        return temp;
     }
 
     public void check() {
--- src/starcraft/StarMain2.java
+++ src/starcraft/StarMain2.java
@@ -17,8 +17,42 @@ public class StarMain2 {
         Marine m3 = new Marine();
         Marine m4 = new Marine();
         Marine m5 = new Marine();
+        System.out.println("m5: " + m5);
 
         Dropship dropship = new Dropship();
+        dropship.load(t1);
+        dropship.load(t2);
+        dropship.load(t3);
+        dropship.load(t4);
+        dropship.load(m1);
+        dropship.load(m2);
+        dropship.load(m3);
+        dropship.load(m4);
+        dropship.load(m5);
+        dropship.check();
+
+        System.out.println("-----------");
+        Unit u1 = dropship.unload2(2);
+        System.out.println("u1: " + u1);
+        dropship.check();
+
+        System.out.println("-----------");
+        Unit u2 = dropship.unload2(2);
+        System.out.println("u2: " + u2);
+        dropship.check();
+
+        System.out.println("-----------");
+        Unit u3 = dropship.unload2(5);
+        System.out.println("u3: " + u3);
+        dropship.check();
+
+        System.out.println("-----------");
+        dropship.load(u3);
+        dropship.check();
+
+        System.out.println("-----------");
+        Unit u4 = dropship.unload2(5);
+        System.out.println("u4: " + u4);
         dropship.check();
     }
 }
