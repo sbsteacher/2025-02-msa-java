6fa65f49925f04ce1fb98bd4b345a02d5ea2831c
sbsteacher <id@gmail.com>
2026-01-23 14:55:50 +0900

    260123 람다식-3

--- src/ch16/sec02/LambdaExample.java
+++ src/ch16/sec02/LambdaExample.java
@@ -5,7 +5,36 @@ public class LambdaExample {
         //Person 객체화
         Person p = new Person();
         //action을 호출하면서 Workable을 implement한 객체 주소값을 전달하시오.
-        p.action();
+        //1번째 (이름있는) 클래스 이용
+        Workable w1 = new Mechanic();
+        p.action(w1);
 
+        //2번째 (익명) 클래스 이용
+        Workable w2 = new Workable() {
+            @Override
+            public void work() {
+                System.out.println("청소부가 청소를 열심히 한다.");
+            }
+        };
+        p.action(w2);
+        p.action(new Workable() {
+            @Override
+            public void work() {
+                System.out.println("청소부가 청소를 열심히 한다.2");
+            }
+        });
+
+        //3번째 람다식 이용
+        Workable w3 = () -> System.out.println("강사가 열심히 강의를 한다.");
+        p.action(w3);
+        p.action(() -> System.out.println("강사가 열심히 강의를 한다.2"));
+    }
+}
+
+
+class Mechanic implements Workable {
+    @Override
+    public void work() {
+        System.out.println("정비공이 정비를 합니다.");
     }
 }
